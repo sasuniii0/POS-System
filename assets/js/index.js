f57f7c409a335749fb048c3580b521438dc037f9
sasuniii0 <sasuniwijerathne@gmail.com>
2025-05-04 11:40:26 +0530

    Add item and order page loading functionality with corresponding controllers

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,21 @@ function loadCustomerPage(){
         })
         .catch(error => console.error('Error loading customer page:', error));
 }
+
+function loadItemPage(){
+    fetch('pages/item.html')
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('section').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading item page:', error));
+}
+
+function loadOrderPage(){
+    fetch('pages/order.html')
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('section').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading order page:', error));
+}
