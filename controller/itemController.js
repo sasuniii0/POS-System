e7acf370fd84b3f6758c5ca9ef87345fe3a97b7e
sasuniii0 <sasuniwijerathne@gmail.com>
2025-05-08 15:57:54 +0530

    Add item model and update item controller and HTML with new functionality

---
+++ controller/itemController.js
@@ -0,0 +1,2 @@
+import ItemModel from "../model/itemModel.js";
+import {item_db} from "../db/db.js";
---
+++ model/itemModel.js
@@ -0,0 +1,8 @@
+export default class ItemModel{
+    constructor( description , unitPrice, quantity, picture ) {
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+        this.picture = picture;
+    }
+}
